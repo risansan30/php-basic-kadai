43de3313789c357713d09b1b8ea55ed3fcb28785
risansan30 <fantianlizi3@gmail.com>
2025-01-30 17:35:57 +0900

    kadai_007フォルダを追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang='ja'>
+
+<head>
+   <mata charset='UTF-8'>
+    <title>PHP課題</title>
+
+</head>
+<body>
+<p>
+    <?php
+$item =['name'=>'onion','price'=> 200,'weight'=> 160];
+print_r($item);
+    ?>
+</p>
+</body>
+    </html>
